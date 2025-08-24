6d674aedcb408e36e0bd52fa34285d53cbdbed70
yourfather <okuhlexesibe0@gmail.com>
2025-08-24 23:46:10 +0200

    Day 2 of fully dedicating my life to Golang

---
+++ goconcurrency/timeOuts.go
@@ -0,0 +1,45 @@
+package main
+
+// Example program to show how to implement timeouts using select and time.After
+// function.
+// The program starts two goroutines that simulate work by sleeping for 2 seconds
+// before sending a result on a channel. The main function uses select statements
+// to wait for either the result from the goroutine or a timeout signal from
+// time.After. The first select has a timeout of 1 second, so it will print
+// "timeout 1". The second select has a timeout of 3 seconds, so it
+// will successfully receive the result from the goroutine and print "result 2".
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+
+    c1 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c1 <- "result 1"
+    }()
+
+    select {
+
+    case res := <-c1:
+        fmt.Println(res)
+    case <-time.After(1 * time.Second):
+
+        fmt.Println("timeout 1")
+    }
+
+    c2 := make(chan string, 1)
+    go func() {
+        time.Sleep(2 * time.Second)
+        c2 <- "result 2"
+    }()
+    select {
+    case res := <-c2:
+        fmt.Println(res)
+    case <-time.After(3 * time.Second):
+        fmt.Println("timeout 2")
+    }
+}
