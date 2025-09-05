135489f804be1f8b5d433f089e0a45de3e648258
yourfather <okuhlexesibe0@gmail.com>
2025-09-05 20:42:22 +0200

    Hello world ..first commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+//This is how you print hello world in go....
+import "fmt"
+func hell(){ 
+	fmt.Println("Hello from me to you!")
+}
+
+/**
+uncomment me!!!
+func main(){ 
+	hell()
+} */
--- test.go
+++ test.go
@@ -1,8 +1,10 @@
 package main
 
+//Rename test to main  and go run 
+
 import "fmt"
 
-func main() {
+func test() {
 
 	i := 10
 
