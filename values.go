8afc923ca76bb3378a9a4179ddbae9970a35dc27
yourfather <okuhlexesibe0@gmail.com>
2025-09-05 20:51:15 +0200

    Basic go values ...second commit

---
+++ values.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+func values(){
+	//Strings .. basic strings concat
+	fmt.Println("Go"+"lang")
+	
+	//Integers .. basic math
+	fmt.Println("1+1 = ", 1+1)
+	//Floats are floats
+	fmt.Println("7.0+2.0 = ", 1+1)
+
+	//Booleans.. basic operations 
+	fmt.Println(false && true) //This is false
+	fmt.Println(false ||  true) //This is true
+	fmt.Println(!true) //negation (outputs false)
+}
+/**
+UNCOMMENT  ME 
+func main(){ 
+	values()
+}*/
