8504e5a6d4ad4253f1c048579ca641e156cc622d
yourfather <okuhlexesibe0@gmail.com>
2025-09-13 00:46:08 +0200

    Hello again

--- http/server.go
+++ http/server.go
@@ -11,6 +11,21 @@ func main() {
 		fmt.Fprintln(w, "Hello, X! Your Go server works 🚀")
 	})
 
+	//Just you looking for some hoes...
+	http.HandleFunc("/hoes" , func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "No hoes available for you man sorry!")
+	})
+
+	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+    name := r.URL.Query().Get("name")
+    if name == "" {
+        name = "stranger"
+    }
+   		fmt.Fprintf(w, "Hello, %s!", name)
+	})
+
+
+
 	// Start the server on port 8080
 	fmt.Println("Server running on http://localhost:8080/hello")
 	http.ListenAndServe(":8080", nil)
